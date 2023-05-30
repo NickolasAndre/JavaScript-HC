7846bcc9b439c015d0ce7c9acc91e385366b9d3e
Nickolas Andre <nickolas.sam3@gmail.com>
2023-05-29 21:05:44 -0300

    commit

---
+++ JS-HC-4-variaveis/script.js
@@ -0,0 +1,37 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'nickolas'
+
+console.log(teste);
+
+teste1 = 'nickolas1';
+
+console.log(teste1)
+
+var $nome = 'dada';
+
+console.log($nome);
+
+var _nome = 'ddaa';
+
+console.log(_nome);
+
+var meuPrimeiroNome = 'eu';
+
+console.log(meuPrimeiroNome);
+
+let testando = 1;
+const ola =2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = "nickolas";
+
+console.log(meuNome);
