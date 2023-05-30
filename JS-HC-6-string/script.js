ed8d59cf8ea7ba378efa631c22c4c43e1977b2e6
Nickolas Andre <nickolas.sam3@gmail.com>
2023-05-30 19:04:56 -0300

    commit

---
+++ JS-HC-6-string/script.js
@@ -0,0 +1,24 @@
+var nome = "Nickolas";
+var sobrenome = 'Andre';
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log(typeof "3.321");
+
+var frase = 'Esta é uma frase';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome+ " " +frase);
+
+document.write("Olá");
+
+console.log("Este número: " + "4231");
+
+console.log("Este número: " + nome);
