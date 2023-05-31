bea4e1ab9dc5025f413b2255d4faa6bbf3ad506e
Nickolas Andre <nickolas.sam3@gmail.com>
2023-05-31 18:21:22 -0300

    commit

---
+++ JS-HC-8-null-undefined/script.js
@@ -0,0 +1,13 @@
+//Hoisting - içamento
+console.log(sobrenome);
+
+var nome = null;
+var sobrenome = 'andre'; //undefined
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = 'nickolas';
+
+console.log(nome);
+
