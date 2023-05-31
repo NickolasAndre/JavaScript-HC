0a9d8cd11e1ae885cb68385227ca32fcb96a1a4f
Nickolas Andre <nickolas.sam3@gmail.com>
2023-05-31 19:55:44 -0300

    commit

---
+++ JS-HC-10-arrays/script.js
@@ -0,0 +1,18 @@
+var arr = [5, 'nickolas', true, {teste: 11, teste:2}]; // obj => {}
+
+console.log(arr);
+
+var arr2 = [1,2,3,4,5];
+
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr[2]);
+
+arr[4] = 10;
+
+arr[0] = 321;
+
+console.log(arr);
+
+console.log(typeof arr);
