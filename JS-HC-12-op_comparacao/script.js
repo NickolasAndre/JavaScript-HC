f886c733509da4436bd6e7799fcf2dd5b2bbdb31
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-01 21:28:13 -0300

    commit

---
+++ JS-HC-12-op_comparacao/script.js
@@ -0,0 +1,15 @@
+var idade = 11;
+
+if(idade >= 18){
+    console.log('pode fazer a carteira');
+}
+
+if(idade <= 17){
+    console.log('não pode fazer a carteira');
+}
+
+var nome = 'nickolas';
+
+if(nome == 'nickolas'){
+    console.log('seu nome é nickolas')
+}
