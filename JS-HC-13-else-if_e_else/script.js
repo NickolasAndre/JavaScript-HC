b1f828d15dca9c71bfd0e4ea12d5da6fdb817d53
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-02 21:43:09 -0300

    commit

---
+++ JS-HC-13-else-if_e_else/script.js
@@ -0,0 +1,18 @@
+var nome = 'andre';
+
+
+if( nome == 'andre'){
+    console.log('Nome é andre')
+}else if(nome == 'nickolas'){
+    console.log('nome é nickolas')
+}else{
+    console.log('Ele possui outro nome!')
+}
+
+var idade = 19;
+
+if(idade > 20){
+    console.log('pode entrar')
+}else if(idade >= 18){
+    console.log('só com autorização')
+}
