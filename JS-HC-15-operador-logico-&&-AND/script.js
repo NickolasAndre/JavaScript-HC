2419be1294663a6a43203b84707e985e2d146d66
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-05 21:44:08 -0300

    commit

---
+++ JS-HC-15-operador-logico-&&-AND/script.js
@@ -0,0 +1,14 @@
+var idade = 16;
+var nome = 'João';
+
+if(idade == 16 && nome == 'João'){
+    console.log('João pode entrar');
+}else{
+    console.log('Este não é o João')
+}
+
+if((1 == 1 && 3 > 3) && true){
+    console.log('Passou');
+}else if(nome === 'João' && idade >= 14){
+    console.log('Aqui passa!!')
+}
