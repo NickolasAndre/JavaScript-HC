6694cc5c6e7915270ef696ed52e00989da3749e9
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-06 21:39:17 -0300

    commit

---
+++ JS-HC-16-operador-logico-OR/script.js
@@ -0,0 +1,16 @@
+var idade = 16;
+var nome = 'joão';
+
+if(nome == 'joão' || idade > 14){
+    console.log('pode entrar');
+}
+/*
+if(nome == 'joão' && 15 > 20 || 10 ==10){
+    console.log('testando');
+}*/
+
+if((nome == 'pedro' || 30 > 20) && 10 ==10){
+    console.log('tentando2');
+}else{
+    console.log('não entrou!');
+}
