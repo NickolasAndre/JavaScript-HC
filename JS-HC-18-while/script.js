e97f623ac0efa32a3d4af78ae98e1cdfbe9616ed
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-09 21:48:28 -0300

    commite

---
+++ JS-HC-18-while/script.js
@@ -0,0 +1,25 @@
+var x = 0;
+
+while(x < 5){
+    console.log('testando' + x)
+
+    //incremento
+
+    x++;
+}
+
+var arr = ['testa', 'testando', 'a', 'b']
+var y = 0;
+
+while(y <= 3){
+    console.log(arr[y]);
+    y++;
+}
+
+var palavra = 'nickolas'
+var i = 0;
+
+while(i <= 8){
+    console.log(palavra[i]);
+    i++;
+}
