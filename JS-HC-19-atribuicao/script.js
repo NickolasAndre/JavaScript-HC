807badb8a061c17803ccc841c757df68f1ac7ba2
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-10 21:55:37 -0300

    commit

---
+++ JS-HC-19-atribuicao/script.js
@@ -0,0 +1,31 @@
+var x = 1;
+var y = 2;
+
+console.log( x = x + y);
+console.log( x += y);
+
+console.log(x -= y);
+
+console.log(x *= y);
+
+console.log(x /= y);
+
+console.log(x ++);
+console.log(x --);
+
+while(x <= 100){
+
+    console.log(x);
+
+    x *= 2;
+}
+console.log(x);
+
+var j = 5;
+
+while(x > 0){
+
+    console.log(x);
+
+    x -= j;
+}
