9af2845e2bb8111770c4140199def7d5995a21f5
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-27 22:10:40 -0300

    commit

---
+++ JS-HC-26-metodos-numericos/script.js
@@ -0,0 +1,19 @@
+//parseFloat 10.0
+
+console.log(parseFloat('12.999'));
+console.log(Number.parseFloat('12.999'));
+
+//parseInt
+
+console.log(parseInt('10'));
+console.log(parseInt(10.98));
+
+//toFixed
+
+console.log(25.3232.toFixed(1));
+
+//isNaN
+
+console.log(isNaN('teste'));
+console.log(isNaN('25'));
+console.log(isNaN(25));
