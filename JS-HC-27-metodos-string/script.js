00ea75a673b43fe32eccead5a6f40d9b2c625a65
Nickolas Andre <nickolas.sam3@gmail.com>
2023-06-29 22:09:44 -0300

    commit-string

---
+++ JS-HC-27-metodos-string/script.js
@@ -0,0 +1,23 @@
+// length
+
+var nome = 'nickolas';
+
+console.log(nome.length);
+
+//indexOf
+
+var frase = 'O rato roeu a roupa do rei de Roma';
+
+console.log(frase.indexOf('roeu'));
+
+//slice
+
+var roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+//replace
+
+var novaFrase = frase.replace('roeu', 'teste');
+
+console.log(novaFrase)
