daa1b4bab6dca027657f030697c74ffce6481919
Nickolas Andre <nickolas.sam3@gmail.com>
2023-07-04 21:44:21 -0300

    commit

---
+++ JS-HC-27-metodos-string-pt2/script.js
@@ -0,0 +1,35 @@
+// toLowerCase e toUpperCase
+
+var frase = 'Esta é a frase que vamos manipular';
+
+var fraseCaixaAlta = frase.toUpperCase();
+
+console.log(fraseCaixaAlta);
+
+console.log(fraseCaixaAlta.toLowerCase());
+
+
+//trim
+
+var nome = '       nickolas      ';
+
+var nomeTrim = nome.trim();
+
+
+console.log(nome);
+console.log(nomeTrim);
+
+
+//split 
+
+console.log(frase.split(' '));
+
+var tags = 'PHP, JavaScript, HTML, CSS';
+
+console.log(tags.split(', '));
+
+var fraseDois = 'Eu quero a última palavra teste desse teste';
+
+console.log(fraseDois.indexOf('teste'));
+
+console.log(fraseDois.lastIndexOf('teste'));
