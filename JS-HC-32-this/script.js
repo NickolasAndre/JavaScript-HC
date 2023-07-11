27d6471d73cfbd7759948885152747a5751785d2
Nickolas Andre <nickolas.sam3@gmail.com>
2023-07-11 20:50:28 -0300

    this

---
+++ JS-HC-32-this/script.js
@@ -0,0 +1,42 @@
+var teste = 5
+
+console.log(this.teste);
+console.log(teste);
+
+
+console.log(this);
+
+let pessoa = {
+
+    nome: 'Nickolas',
+    idade: 24,
+    falar: function(){
+        console.log('Olá,tudo bem?');
+    },
+
+    dizerNome: function(){
+        console.log('O meu nome é: ' + this.nome);
+    },
+
+    aniversario: function(){
+        this.idade += 1;
+    },
+    saudacao: function(){
+        return 'Sr. ' + this.nome
+    }
+};
+pessoa.dizerNome();
+
+console.log(pessoa.idade);
+
+pessoa.aniversario();
+
+console.log(pessoa.idade);
+
+pessoa.saudacao();
+
+console.log(pessoa.saudacao())
+
+var sdc = pessoa.saudacao();
+
+console.log('Olá ' + sdc)
