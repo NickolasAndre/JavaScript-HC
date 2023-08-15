62ad71493e9c6d65c74caf51079d1db10f139f41
Nickolas Andre <nickolas.sam3@gmail.com>
2023-08-14 21:23:58 -0300

    removeChild-e-remove

---
+++ JS-HC-37-remover-elementos/script.js
@@ -0,0 +1,11 @@
+// removendo elemento filho
+var container = document.querySelector('#container');
+
+var p = document.querySelector('#container p');
+container.removeChild(p);
+
+// remover elemento
+
+var subtitle = document.querySelector('.subtitle');
+//var h2 = document.querySelector('.subtitle h2'); funcionar também
+subtitle.remove()//(h2)
