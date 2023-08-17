52af649405cfc3837dfe30ac188a34350106e5d1
Nickolas Andre <nickolas.sam3@gmail.com>
2023-08-17 18:46:17 -0300

    Adicionar-CSS

---
+++ JS-HC-41-adicionar-CSS/script.js
@@ -0,0 +1,19 @@
+// selecionar elemento
+
+var title = document.querySelector("#title");
+
+// adicionar estilo
+
+title.style.color = "blue";
+
+// background-color
+
+title.style.backgroundColor = "yellow";
+
+// selecionar elementos
+
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar vários estilos
+
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
