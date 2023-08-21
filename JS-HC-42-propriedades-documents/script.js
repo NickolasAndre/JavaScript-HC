8c3fd38eb44cbcb24b02753de7279eefb6ec7f6b
Nickolas Andre <nickolas.sam3@gmail.com>
2023-08-21 18:33:49 -0300

    propriedades-document

---
+++ JS-HC-42-propriedades-documents/script.js
@@ -0,0 +1,21 @@
+// propriedades document
+
+console.log(document.body);
+
+console.log(document.links[0]);
+
+//para alterar
+
+document.links[0].textContent = "Instagram"
+
+// URL
+
+console.log(document.URL);
+
+// o title
+
+console.log(document.title);
+
+document.title = "Aula 42"
+
+console.log(document.title);
